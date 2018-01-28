2f8eba3c33ed3794b09ecca0ac9a955c4f950c73
Angelo Aracri <angeloaracri@yahoo.de>
2018-01-28 10:16:27 +0100

    Setting some more values in Country

--- scripts/model/Country.gd
+++ scripts/model/Country.gd
@@ -1,18 +1,26 @@
 var name = ""
 
+const MIN_INHABITANTS = 10
+const MAX_INHABITANTS = 100
 
+const MIN_RELATIONSHIP = 0.25
+const MAX_RELATIONSHIP = 0.75
+
+var inhabitants = 0
 var params = {
     "economy" : 0.5,
     "satisfaction" : 0.5,
     "xenophobia" : 0.5
 }
-var relationships = {}
+var readers = 0.1 # between 0 and 1, representing fraction of inhabitants
+var relationships = {} # between 0 and 1, 1 for each other country
 
 func _init(name):
     self.name = name
 
 func prepare(world):
+    inhabitants = randi() % (MAX_INHABITANTS - MIN_INHABITANTS) + MIN_INHABITANTS
     for c in world.countries:
         if c.name == self.name:
             continue
-        relationships[c.name] = randf() / 2 + 0.25
+        relationships[c.name] = randf() * (MAX_RELATIONSHIP - MIN_RELATIONSHIP) + MIN_RELATIONSHIP
