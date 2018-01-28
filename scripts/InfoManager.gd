31527d9947da400a1aac48719419cd598b08e41f
Matthias Schmitt <male.schmitt@posteo.de>
2018-01-28 13:30:52 +0100

    Tweetr and Fax fix and abstraction

--- scripts/InfoManager.gd
+++ scripts/InfoManager.gd
@@ -6,5 +6,6 @@ const InfoScene = preload("res://scenes/Info.tscn")
 func _on_Main_message_arrived(message):
     if message.type == Message.NOTE:
         var info_node = InfoScene.instance()
-        get_node("/root/Main/Draggables").add_child(info_node)
         info_node.init(message)
+        get_node("/root/Main/Draggables").add_child(info_node)
+
