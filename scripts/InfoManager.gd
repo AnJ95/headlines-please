f8ff9660153d89680183f4b8b19ad2307f796c80
Matthias Schmitt <male.schmitt@posteo.de>
2018-01-28 12:28:57 +0100

    Only nodes are nodes

--- scripts/InfoManager.gd
+++ scripts/InfoManager.gd
@@ -4,8 +4,7 @@ const Message = preload("res://scripts/model/Message.gd")
 const InfoScene = preload("res://scenes/Info.tscn")
 
 func _on_Main_message_arrived(message):
-    if message.type == Message.TWEET:
-        return
-    var info_node = InfoScene.instance()
-    get_node("/root/Main/Draggables").add_child(info_node)
-    info_node.init(message)
+    if message.type == Message.NOTE:
+        var info_node = InfoScene.instance()
+        get_node("/root/Main/Draggables").add_child(info_node)
+        info_node.init(message)
