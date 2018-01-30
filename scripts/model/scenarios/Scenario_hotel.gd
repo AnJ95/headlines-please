78462c1125778fdd593adc688f74fc94e236627e
Angelo Aracri <angeloaracri@yahoo.de>
2018-01-30 01:13:13 +0100

    Fixed {country_*} to be in lowercase

--- scripts/model/scenarios/Scenario_hotel.gd
+++ scripts/model/scenarios/Scenario_hotel.gd
@@ -15,21 +15,21 @@ func get_propability(world, countries):
 #TODO set an effect for each Headline
 
 func init_headlines():
-    headlines.append(Headline.new("{Country_1} allows people to treat citizens of {Country_2} derogatory.",0.19,0.50,0.75))
-    headlines.append(Headline.new("Racist {Country_1}ian hotelowner Eduard Glitz bans other nations from entering.",0.38,0.40,0.65))
-    headlines.append(Headline.new("Controversial {Country_1}ian hotelowner bans group of misbehaving foreigners.",0.55,0.55,0.42))
-    headlines.append(Headline.new("Enraged {Country_1}ian hotelowner throws out a group of vandalizing {Country_2}ian visitors.",0.60,0.60,0.40))
-    headlines.append(Headline.new("Drunken {Country_2}ian hooligans wreck Glitz hotel, get banned.",0.55,0.80,0.65))
+    headlines.append(Headline.new("{country_1} allows people to treat citizens of {country_2} derogatory.",0.19,0.50,0.75))
+    headlines.append(Headline.new("Racist {country_1}ian hotelowner Eduard Glitz bans other nations from entering.",0.38,0.40,0.65))
+    headlines.append(Headline.new("Controversial {country_1}ian hotelowner bans group of misbehaving foreigners.",0.55,0.55,0.42))
+    headlines.append(Headline.new("Enraged {country_1}ian hotelowner throws out a group of vandalizing {country_2}ian visitors.",0.60,0.60,0.40))
+    headlines.append(Headline.new("Drunken {country_2}ian hooligans wreck Glitz hotel, get banned.",0.55,0.80,0.65))
 
 
 func init_messages():
-    messages.append(Message.new(Message.TWEET, [0,1], "The {Country_2}ians were cruely beat up by hotel security!"))
-    messages.append(Message.new(Message.TWEET, [4], "The {Country_2}ians allegedly snuck in and plundered the bar.", 0.38))
+    messages.append(Message.new(Message.TWEET, [0,1], "The {country_2}ians were cruely beat up by hotel security!"))
+    messages.append(Message.new(Message.TWEET, [4], "The {country_2}ians allegedly snuck in and plundered the bar.", 0.38))
     messages.append(Message.new(Message.TWEET, [1], "Eduard Glitz is known to be a racist, isn't he?", 0.55))
 
-    messages.append(Message.new(Message.NOTE, [3,4], "{Country_2}ians cause $15,000 damage at hotel."))
-    messages.append(Message.new(Message.NOTE, [0,1], "Hotelowner Eduard Glitz forcefully removed group of {Country_2}ians."))
-    messages.append(Message.new(Message.NOTE, [1,2], "The misbehaving {Country_2}ians recieved a ban from the Glitz hotels."))
+    messages.append(Message.new(Message.NOTE, [3,4], "{country_2}ians cause $15,000 damage at hotel."))
+    messages.append(Message.new(Message.NOTE, [0,1], "Hotelowner Eduard Glitz forcefully removed group of {country_2}ians."))
+    messages.append(Message.new(Message.NOTE, [1,2], "The misbehaving {country_2}ians recieved a ban from the Glitz hotels."))
     messages.append(Message.new(Message.NOTE, [2,3], "A group of hooligans caused $8,000 damage at the couches of a hotel lounge."))
     messages.append(Message.new(Message.NOTE, [3,4], "The hotel vandals were unemployed and at the time heavily intoxicated."))
 	
--- scripts/model/scenarios/Scenario_practice_flight.gd
+++ scripts/model/scenarios/Scenario_practice_flight.gd
@@ -15,25 +15,25 @@ func get_propability(world, countries):
 #TODO set an effect for each Headline
 
 func init_headlines():
-    headlines.append(Headline.new("Training flight of {Country_1} went wrong, causing international conflict.",0.35,0.42,0.70))
-    headlines.append(Headline.new("{Country_1}s military pilots accidentaly crossed border on training flight.",0.40,0.50,0.60))
-    headlines.append(Headline.new("{Country_1}s army made mistake while drill. Pilot crossing border of {Country_2}.",0.65,0.50,0.40))
-    headlines.append(Headline.new("{Country_1}s army is advancing over border of {Country_2}",0.55,0.70,0.50))
-    headlines.append(Headline.new("{Country_1}s army provoked {Country_2} with drill mistake at border.",0.37,0.60,0.80))
+    headlines.append(Headline.new("Training flight of {country_1} went wrong, causing international conflict.",0.35,0.42,0.70))
+    headlines.append(Headline.new("{country_1}s military pilots accidentaly crossed border on training flight.",0.40,0.50,0.60))
+    headlines.append(Headline.new("{country_1}s army made mistake while drill. Pilot crossing border of {country_2}.",0.65,0.50,0.40))
+    headlines.append(Headline.new("{country_1}s army is advancing over border of {country_2}",0.55,0.70,0.50))
+    headlines.append(Headline.new("{country_1}s army provoked {country_2} with drill mistake at border.",0.37,0.60,0.80))
 
 
 func init_messages():
     messages.append(Message.new(Message.TWEET, [4], "Crazy pilot provoking war!"))
     messages.append(Message.new(Message.TWEET, [0,4], "Was the political conflict at the border really an accident?.", 0.38))
-    messages.append(Message.new(Message.TWEET, [1,2,3], "{Country_1}s pilot flight over border was only a mistake.", 0.55))
+    messages.append(Message.new(Message.TWEET, [1,2,3], "{country_1}s pilot flight over border was only a mistake.", 0.55))
 
-    messages.append(Message.new(Message.NOTE, [1,3], "{Country_1} has conducted a practice flight at the border to {Country_2}."))
-    messages.append(Message.new(Message.NOTE, [3], "{Country_1} prepares for possible war by mobilizing troupes at the border of {Country_2}."))
-    messages.append(Message.new(Message.NOTE, [1], "{Country_1} drills units against possible invasion of {Country_2}."))
-    messages.append(Message.new(Message.NOTE, [0,4], "{Country_1} deliberately accepts accidentaly provoking {Country_2}."))
-    messages.append(Message.new(Message.NOTE, [1,2,3,4], "{Country_1}ian jet planes accidentaly flew over border of {Country_2}."))
+    messages.append(Message.new(Message.NOTE, [1,3], "{country_1} has conducted a practice flight at the border to {country_2}."))
+    messages.append(Message.new(Message.NOTE, [3], "{country_1} prepares for possible war by mobilizing troupes at the border of {country_2}."))
+    messages.append(Message.new(Message.NOTE, [1], "{country_1} drills units against possible invasion of {country_2}."))
+    messages.append(Message.new(Message.NOTE, [0,4], "{country_1} deliberately accepts accidentaly provoking {country_2}."))
+    messages.append(Message.new(Message.NOTE, [1,2,3,4], "{country_1}ian jet planes accidentaly flew over border of {country_2}."))
 	
-    messages.append(Message.new(Message.PHAX, [1,2], "{Country_1} ordered only a standard training flight at the border to {Country_2}.", 0.25))
-    messages.append(Message.new(Message.PHAX, [2], "One of the {Country_1} pilots flew off to wrong coordinates after radio miscommunication.", 0.48))
-    messages.append(Message.new(Message.PHAX, [0,2], "{Country_1}s generals have issued an official appology on the matter.", 0.66))
+    messages.append(Message.new(Message.PHAX, [1,2], "{country_1} ordered only a standard training flight at the border to {country_2}.", 0.25))
+    messages.append(Message.new(Message.PHAX, [2], "One of the {country_1} pilots flew off to wrong coordinates after radio miscommunication.", 0.48))
+    messages.append(Message.new(Message.PHAX, [0,2], "{country_1}s generals have issued an official appology on the matter.", 0.66))
 	
