6a2b7cb8dd2d40bfa1fe4f3aa461c39f56a2d570
Angelo Aracri <angeloaracri@yahoo.de>
2018-01-31 08:42:17 +0100

    Added Airmail animations

--- scripts/Airmail.gd
+++ scripts/Airmail.gd
@@ -1,11 +1,17 @@
 extends "res://scripts/Droppable.gd"
 
 var minY
-const scrollHeight = 200
 var maxY
+const scrollHeight = 200
+const tween_speed = 100
 var isVacuuming = false
 
+# used for animating left&right on day_end
 onready var animation_player = get_node("AnimationPlayer")
+# used for animating up&down on drag
+onready var tween = get_node("Tween")
+# used for shaking the sprite
+onready var shake_animation_player = get_node("Sprite/AnimationPlayer")
 onready var audio_stream_player = get_node("AudioStreamPlayer")
 onready var audio_stream_player_node = get_node("AudioStreamPlayerNode")
 onready var audio_stream_player_grab = get_node("AudioStreamPlayerGrab")
@@ -26,7 +32,18 @@ func accepts_drops_now():
 func on_enter(draggable):
     audio_stream_player_node.play()
     draggable.get_parent().remove_child(draggable)
+    stop_shaking()
+
+# from Droppable
+func hovering_now(draggable):
+    start_shaking()
+    pass
     
+func not_hovering():
+    stop_shaking()
+    pass
+    
+# from Draggable
 func move_drag():
     rect_position.x = startThisPos.x
     if rect_position.y < minY:
@@ -40,22 +57,41 @@ func move_drag():
             audio_stream_player.play()
         if !audio_stream_player_grab.playing:
             audio_stream_player_grab.play()
-            
-    
-    
+   
+# from Draggable
 func start_drag():
     if !audio_stream_player.playing:
         audio_stream_player.play()
     audio_stream_player_grab.play()
     pass
 
+# from Draggable
 func stop_drag():
     if rect_position.y - minY > scrollHeight / 2:
-        rect_position.y = maxY # TODO animate
-        isVacuuming = true
+        move_in()
     else:
-        rect_position.y = minY # TODO animate
-        isVacuuming = false
-        audio_stream_player.stop()
+        move_out()
     audio_stream_player_grab.stop()
+
+func move_in():
+    var start = rect_position.y
+    tween.interpolate_property(self, "margin_top", start, maxY, (maxY - start) / tween_speed, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+    tween.start()
+    isVacuuming = true
+    
+func move_out():
+    var start = rect_position.y
+    tween.interpolate_property(self, "margin_top", start, minY, (start - minY) / tween_speed, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
+    tween.start()
+    isVacuuming = false
+    audio_stream_player.stop()
+    
+    
+func start_shaking():
+    if !shake_animation_player.is_playing():
+        shake_animation_player.play("shake")
+        
+func stop_shaking():
+    if shake_animation_player.is_playing():
+        shake_animation_player.stop()
     
