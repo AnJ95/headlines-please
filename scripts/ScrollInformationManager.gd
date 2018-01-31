fee88ff37488a469a2e7e73a4935dc4a2e577407
Angelo Aracri <angeloaracri@yahoo.de>
2018-01-31 03:14:01 +0100

    Renamed ScrollMessageManager to ScrollInformationManager

--- scripts/ScrollInformationManager.gd
+++ scripts/ScrollInformationManager.gd
@@ -2,7 +2,7 @@ extends Control
 
 const Message = preload("res://scripts/model/Message.gd")
 
-export(PackedScene) var MessageScene
+export(PackedScene) var InformationScene
 export(Vector2) var anchor_pos
 export(int, "TWEET", "NOTE", "PHAX") var message_type
 export var max_messages = 3
@@ -24,19 +24,18 @@ func remove(node):
     nodes.remove(node)
         
 func add(message):
-    var message_node = MessageScene.instance()
-    message_node.rect_position = anchor_pos
-    message_node.init(message)
-    add_child(message_node)
+    var node = InformationScene.instance()
+    node.rect_position = anchor_pos
+    node.init(message)
+    add_child(node)
     if set_to_back:
-        message_node.get_parent().move_child(message_node, 0)
-    nodes.append(message_node)
-    
+        node.get_parent().move_child(node, 0)
+    nodes.append(node)
     
     audio_stream_player.play()
     
     for n in nodes:
-        n.rect_position -= Vector2(0, message_node.rect_size.y)
+        n.rect_position -= Vector2(0, node.rect_size.y)
     
     while nodes.size() > max_messages:
         nodes.pop_front().queue_free()
