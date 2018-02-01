c1d738dac773f6cb006f3cff4c2b0fd95ad6318d
Angelo Aracri <angeloaracri@yahoo.de>
2018-02-01 16:55:13 +0100

    Made statistically half of all notes appear at start

--- scripts/model/Message.gd
+++ scripts/model/Message.gd
@@ -2,6 +2,7 @@ const FormatUtils = preload("res://scripts/FormatUtils.gd")
 
 enum { TWEET, NOTE, PHAX }
 
+const MIN_RANDOM_ARRIVAL_TIME = -0.3
 const MAX_RANDOM_ARRIVAL_TIME = 0.3
 var type
 var format_string
@@ -15,7 +16,9 @@ func _init(type, headlines, format_string, arrival_time = -1):
     self.headlines = headlines
     self.format_string = format_string
     if arrival_time == -1:
-        self.arrival_time = randf() * MAX_RANDOM_ARRIVAL_TIME
+        self.arrival_time = randf() * (MAX_RANDOM_ARRIVAL_TIME - MIN_RANDOM_ARRIVAL_TIME) + MIN_RANDOM_ARRIVAL_TIME
+        if self.arrival_time < 0:
+            self.arrival_time = 0
     else:
         self.arrival_time = arrival_time
 
